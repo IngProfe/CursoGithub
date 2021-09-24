9bd5364cd19aba268b476d06f42c9be1d74920f6
User <User@DESKTOP-7FKHBUK>
2021-09-24 15:42:36 -0500

    Prueba de Eclipse a github

---
+++ Curso/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hola!!! Buenas Tardes!!!");
+		Perro dog = new Perro();
+		
+		dog.nombre = "Perruno";
+		dog.altura = "98 cms";
+		
+		System.out.println(dog.comer("Atun"));
+	}
+
+}
---
+++ Curso/src/Perro.java
@@ -0,0 +1,28 @@
+
+public class Perro {
+	
+	public String nombre;
+	public String raza;
+	public String altura;
+	
+	public Perro()
+	{
+		
+	}
+	
+	public String comer(String carne)
+	{
+		return nombre + " mide: " + altura + " y comerá: " + carne;
+	}
+	
+	public String dormir()
+	{
+		return "";
+	}
+	
+	public String ladrar()
+	{
+		return "";
+	}
+
+}
